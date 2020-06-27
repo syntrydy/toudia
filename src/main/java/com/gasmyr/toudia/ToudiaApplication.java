d86d75514817290b85758961dfdebd81b9cb1938
Gasmyr <gasmyrmougang@yahoo.fr>
2020-06-27 20:48:28 +0100

    Base setup

---
+++ src/main/java/com/gasmyr/toudia/ToudiaApplication.java
@@ -0,0 +1,13 @@
+package com.gasmyr.toudia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToudiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToudiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gasmyr/toudia/ToudiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasmyr.toudia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToudiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
